e575d3df786875c37dc5ee595a0d0b43c97bb031
JohnRo <46386386+jhonatanrojasbastidas@users.noreply.github.com>
2021-05-04 17:04:22 -0500

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por{" "}
+      <a href="https://www.linkedin.com/in/jhonatanrojasbastidas/">
+        Jhonatan Rojas Bastidas
+      </a>
+    </div>
+    <div>
+      <a href="https://github.com/jhonatanrojasbastidas">Sígueme en Git hub</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
